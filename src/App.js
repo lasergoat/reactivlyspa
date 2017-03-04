a2ae81d5703d7c897cc0fa25ee85f0c02ac920bc
Daniel Walker <polyesterhat@gmail.com>
2017-03-04 13:08:54 -0500

    get app to display slides url from presenter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import io from 'socket.io-client';
 
+import get from 'lodash/get';
+
 import './App.css';
 import './Emoji.css';
 
@@ -10,14 +12,23 @@ import emojis from './emojis-util';
 const socket = io(`http://localhost:3001`)
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      url: null,
+    };
+  }
   componentWillMount() {
     socket.on('event', (data) => {
       console.log(data);
     });
 
-    socket.on("R:App\\Events\\BeginSlides", (message) => {
-        // increase the power everytime we load test route
-        console.log(message)
+    socket.on("R:App\\Events\\BeginSlides", (data) => {
+      // this will be the Slides url
+      console.log(data);
+      this.setState({
+        url: get(data, 'data.url')
+      })
     });
   }
   sendEmoji(emoji) {
@@ -28,12 +39,22 @@ class App extends Component {
     console.log(emoji);
   }
   render() {
+    const {
+      url
+    } = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src="reactivly-logo.png" className="App-logo" alt="logo" />
         </div>
         <div className="App-content">
+          {url ? (
+            <div className="url-wrapper">
+              <h3>Sides:</h3>
+              <a target="_blank" href={url}>{url}</a>
+            </div>
+          ) : null}
           <div className="emoji-wrapper">
             {emojis.map((emoji) => (
               <div
